c22fb67d05af4f353619858a750e350b363d3b9b
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2025-09-06 16:09:29 -0300

    bug prevention

--- src/genetics/crossover/single_point_crossover.py
+++ src/genetics/crossover/single_point_crossover.py
@@ -2,14 +2,14 @@ import random
 from ..utils import blend_colors
 
 def single_point_crossover(parent1, parent2):
-    if len(parent1.polygons) < 2 or len(parent2.polygons) < 2:
-        return parent1.clone(), parent2.clone()
-    
-    min_len = min(len(parent1.polygons), len(parent2.polygons))
-    crossover_point = random.randint(1, min_len - 1)
-
     child1 = parent1.clone()
     child2 = parent2.clone()
+    
+    min_len = min(len(parent1.polygons), len(parent2.polygons))
+    if min_len < 2:
+        return child1, child2
+    
+    crossover_point = random.randint(1, min_len - 1)
 
     child1.polygons = parent1.polygons[:crossover_point] + parent2.polygons[crossover_point:]
     child2.polygons = parent2.polygons[:crossover_point] + parent1.polygons[crossover_point:]
